d5994c80415c3f537f359442cf18cc29d3814d14
FernandoFortunato2020 <65669290+FernandoFortunato2020@users.noreply.github.com>
2023-08-29 15:36:53 -0300

    Update VerbosTest.java

--- src/test/java/VerbosTest.java
+++ src/test/java/VerbosTest.java
@@ -1,24 +1,27 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
-
 @DisplayName("Testes de API Rest")
 public class VerbosTest {
     @Test
     public void testGet() {
         given()
                 .when()
-                .get("https://jsonplaceholder.typicode.com/users")
+                .get("https://jsonplaceholder.typicode.com/users?id=10")
                 .then()
+                .body("name", contains("Clementina DuBuque"))
+                .body("email", contains("Rey.Padberg@karina.biz"))
+                .body("username", contains("Moriah.Stanton"))
+                .body("phone", contains("024-648-3804"))
+                .body("website", contains("ambrose.net"))
                 .statusCode(200);
-
     }
 
     @Test
     public void testPost() {
         given()
-
                 .body("{\n" +
                         "        \"id\": 1,\n" +
                         "        \"name\": \"Fernando Fortunato\",\n" +
@@ -55,13 +58,13 @@ public class VerbosTest {
         given()
                 .body("{\n" +
                         "        \"id\": 1,\n" +
-                        "        \"name\": \"Fernando Fortunato\",\n" +
-                        "        \"username\": \"FerFortunato\",\n" +
-                        "        \"email\": \"fernandofortunato@hotmail.com\",\n" +
+                        "        \"name\": \"Fernando Fortunato Alterado\",\n" +
+                        "        \"username\": \"FerFortunato Alterado\",\n" +
+                        "        \"email\": \"fernandofortunatoalterado@hotmail.com\",\n" +
                         "        \"address\": {\n" +
-                        "            \"street\": \"Frederico Maurer\",\n" +
-                        "            \"suite\": \"Apt. 208\",\n" +
-                        "            \"city\": \"Curitiba\",\n" +
+                        "            \"street\": \"Frederico Maurer Alterado\",\n" +
+                        "            \"suite\": \"Apt. 208 Alterado\",\n" +
+                        "            \"city\": \"Curitiba Alterado\",\n" +
                         "            \"zipcode\": \"81630-020\",\n" +
                         "            \"geo\": {\n" +
                         "                \"lat\": \"-40.7779\",\n" +
@@ -69,25 +72,25 @@ public class VerbosTest {
                         "            }\n" +
                         "        },\n" +
                         "        \"phone\": \"5-123-876-1111 x56442\",\n" +
-                        "        \"website\": \"teste.org\",\n" +
+                        "        \"website\": \"testealterado.org\",\n" +
                         "        \"company\": {\n" +
-                        "            \"name\": \"Parana\",\n" +
+                        "            \"name\": \"Parana Alterado\",\n" +
                         "            \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
                         "            \"bs\": \"harness real-time e-markets\"\n" +
                         "        }\n" +
                         "    }'")
                 .when()
-                .put("https://jsonplaceholder.typicode.com/users")
+                .put("https://jsonplaceholder.typicode.com/users/10")
                 .then()
                 .statusCode(200)
-                .body("id", equalTo(1));
+                .body("id", equalTo(10));
     }
 
     @Test
     public void testDelete() {
         given()
                 .when()
-                .delete("https://jsonplaceholder.typicode.com/users")
+                .delete("https://jsonplaceholder.typicode.com/users/10")
                 .then()
                 .statusCode(200);
     }
